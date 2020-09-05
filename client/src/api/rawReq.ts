ea44538fa720bfae3ef22f79c587d4f56b29c826
Martin Eskdale Moen <martinmoen@gmail.com>
2020-09-05 23:05:36 +0100

    Derive the host from current hostname
    If the REACT_APP_API_BASE_URL is not set it will try and derive the hostname from the current location

--- client/src/api/rawReq.ts
+++ client/src/api/rawReq.ts
@@ -2,7 +2,11 @@
 const rawReq = new Proxy(fetch, {
   apply: (target, _, args) => {
     const opt = args[1] ? args[1] : {};
-    const url = `${process.env.REACT_APP_API_BASE_URL}/${args[0]}`;
+    let host = process.env.REACT_APP_API_BASE_URL;
+    if (!host) {
+      host = `${window.location.hostname}:20080`;
+    }
+    const url = `${host}/${args[0]}`;
     // Call fetch, always assume json.
     return target(url, opt);
   },
