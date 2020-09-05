17ae2977ff93da945d0d7c0b8652ca01108a1f5a
Martin Eskdale Moen <martinmoen@gmail.com>
2020-09-05 23:04:53 +0100

    Removed 'serve' command
    The root command now starts the server

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,8 @@ import (
 	"github.com/spf13/cobra"
 	"os"
 
+	"github.com/botto/dsnet-gui/server"
+
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
@@ -39,6 +41,9 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		server.Start()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -62,6 +67,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	viper.SetDefault("port", "20080")
 }
 
 // initConfig reads in config file and ENV variables if set.
