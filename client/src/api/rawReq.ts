c5fec68ee849816fbcc31cdbc33633a14db2a69e
Martin Eskdale Moen <martinmoen@gmail.com>
2020-09-09 12:50:43 +0100

    Fixing client connection

--- client/src/api/rawReq.ts
+++ client/src/api/rawReq.ts
@@ -4,7 +4,7 @@ const rawReq = new Proxy(fetch, {
     const opt = args[1] ? args[1] : {};
     let host = process.env.REACT_APP_API_BASE_URL;
     if (!host) {
-      host = `${window.location.hostname}:20080`;
+      host = `//${window.location.hostname}:20080/api/v1`;
     }
     const url = `${host}/${args[0]}`;
     // Call fetch, always assume json.
