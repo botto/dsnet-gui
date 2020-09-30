4706e3052bd317ede04e9fa85bd30a48f89b29bd
Martin Eskdale Moen <martinmoen@gmail.com>
2020-09-30 23:15:04 +0100

    Fix CSS background to go all the way down

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Colors } from '@blueprintjs/core';
+
+// Keep this loading order, otherwise blueprint css overides custom styles in our own comps.
 import 'normalize.css/normalize.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
-import { Colors } from "@blueprintjs/core";
-import styles from './styles.module.css';
+
+import './base.css';
+
+// Load custom comps after blueprint css.
 import Overview from './components/Overview';
+import styles from './styles.module.sass';
 
 const App = () => (
   <div className={styles.App} style={{ background: Colors.DARK_GRAY3}}>
