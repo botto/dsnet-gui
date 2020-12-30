7a34d52fed500f0ac852fe41c0647f9650f46c6c
Martin Eskdale Moen <martinmoen@gmail.com>
2020-12-30 20:22:02 +0000

    Let us instantiate a peer

--- client/src/models/peer.ts
+++ client/src/models/peer.ts
@@ -1,11 +1,29 @@
-interface Peer {
-  Hostname   : string;
-  Owner      : string;
-  Description: string;
-  IP         : string;
-  Added      : string;
-  Networks   : string[];
-  PublicKey  : string;
+
+class Peer {
+  readonly Hostname   : string;
+  readonly Owner      : string;
+  readonly Description: string;
+  readonly IP         : string | undefined;
+  readonly Added      : string | undefined;
+  readonly Networks   : string[] | undefined;
+  readonly PublicKey  : string | undefined;
+  constructor(
+    hostName: string,
+    owner: string,
+    description: string,
+    ip?: string,
+    added?: string,
+    networks?: string[],
+    publicKey?: string,
+  ) {
+    this.Hostname = hostName;
+    this.Owner = owner;
+    this.Description = description;
+    this.IP = ip;
+    this.Added = added;
+    this.Networks = networks;
+    this.PublicKey = publicKey;
+  }
 }
 
 export default Peer;
