582dcc6ed1ef66a54ede3469e234b7e272d3e7df
Martin Eskdale Moen <martinmoen@gmail.com>
2020-12-30 20:24:02 +0000

    Make sure all elements have BP styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Colors } from '@blueprintjs/core';
 
-// Keep this loading order, otherwise blueprint css overides custom styles in our own comps.
-import 'normalize.css/normalize.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import '@blueprintjs/core/lib/css/blueprint.css';
-
 import './base.css';
 
 // Load custom comps after blueprint css.
@@ -14,7 +9,7 @@ import styles from './styles.module.sass';
 
 const App = () => (
   <div className={styles.App} style={{ background: Colors.DARK_GRAY3}}>
-    <div className={ `${styles.Container} bp3-dark`}>
+    <div className={ `${styles.Container}`}>
       <Overview />
     </div>
   </div>
