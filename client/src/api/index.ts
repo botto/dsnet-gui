202790381e3a0cbb4c8e28ede6dd841a41841f54
Martin Eskdale Moen <martinmoen@gmail.com>
2021-01-01 17:17:00 +0000

    Make sure api errors are thrown to the caller

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,11 +1,12 @@
 
-import DSNetReport from '../models/dsnet_report';
 import Peer from '../models/peer';
 import TimeSeries from '../models/time_series';
-import TimeSeriesResponse from '../models/time_series_response';
 import rawReq from './rawReq';
 import {
-  AddPeerResponse, NewPeerPayload
+  AddPeerResponse,
+  ErrorResponse,
+  NewPeerPayload,
+  ReportResponse
 } from './types';
 
 enum GetEndpoint {
@@ -14,11 +15,6 @@ enum GetEndpoint {
 
 enum PostEndpoint {
   addPeer = 'peer'
-}
-
-interface ReportResponse {
-  Report: DSNetReport,
-  TimeSeries: TimeSeriesResponse,
 }
 
 type ResponseTypes<E> =
@@ -30,61 +26,64 @@ type PayloadType<E> =
   E extends PostEndpoint.addPeer ? NewPeerPayload :
   never;
 
-const doGet = <R extends GetEndpoint>(endpoint: R): Promise<ResponseTypes<R>> =>
-  rawReq(endpoint)
-    .then(resp => {
-      if (resp.status >= 200 && resp.status < 300) {
-        return resp.json();
-      }
-      throw new Error('Did not get 2xx response from server');
-    })
-    .then(d => d as ResponseTypes<R>);
 
-const doPost = <R extends PostEndpoint>(endpoint: R, payLoad: PayloadType<R>): Promise<ResponseTypes<R>> =>
-    rawReq(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payLoad),
-    })
-      .then(resp => {
-        if (resp.status >= 200 && resp.status < 300) {
-          return resp.json();
-        }
-        console.log(`Response: ${resp}`);
-        throw new Error('Did not get 2xx response from server');
-      })
-      .then(d => d as ResponseTypes<R>);
+const doGet = async <R extends GetEndpoint>(endpoint: R): Promise<ResponseTypes<R>> => {
+  const resp = await rawReq(endpoint);
+  const respData = await resp.json()
+  if (resp.status >= 200 && resp.status < 300) {
+    return respData as ResponseTypes<R>
+  }
+  else {
+    const errData = respData as ErrorResponse;
+    if (errData.Error !== '') {
+      throw new Error(errData.Error);
+    }
+    else {
+      throw new Error('Unknown error');
+    }
+  }
+};
+
+const doPost = async <R extends PostEndpoint>(endpoint: R, payLoad: PayloadType<R>): Promise<ResponseTypes<R>> => {
+  const resp = await rawReq(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payLoad),
+  });
+  const respData = await resp.json();
+  if (resp.status >= 200 && resp.status < 300) {
+    return respData as ResponseTypes<R>;
+  }
+  else {
+    const errData = respData as ErrorResponse;
+    if (errData.Error !== '') {
+      throw new Error(errData.Error);
+    }
+    else {
+      throw new Error('Unknown error');
+    }
+  }
+};
 
 /***** GET Endpoints *****/
 const getReport = async () => {
-  try {
-    const data = await doGet(GetEndpoint.report);
-    return {
-      TimeSeries: new TimeSeries(data.TimeSeries),
-      Report: data.Report,
-    };
-  } catch (err) {
-    console.error(`failed getting report data - ${err}`);
-    throw err;
-  }
+  const data = await doGet(GetEndpoint.report);
+  return {
+    TimeSeries: new TimeSeries(data.TimeSeries),
+    Report: data.Report,
+  };
 }
 
 const postAddPeer = async (newPeer: Peer): Promise<AddPeerResponse> => {
-  try {
-    const payload: NewPeerPayload = {
-      Owner: newPeer.Owner,
-      Hostname: newPeer.Hostname,
-      Description: newPeer.Description,
-    };
-    const data = await doPost(PostEndpoint.addPeer, payload);
-    return {Conf: data.Conf};
-  }
-  catch (err) {
-    console.error(`failed creating new peer - ${err}`);
-    throw err;
-  }
+  const payload: NewPeerPayload = {
+    Owner: newPeer.Owner,
+    Hostname: newPeer.Hostname,
+    Description: newPeer.Description,
+  };
+  const data = await doPost(PostEndpoint.addPeer, payload);
+  return { Conf: data.Conf };
 }
 
 export {
--- client/src/api/types.ts
+++ client/src/api/types.ts
@@ -1,3 +1,6 @@
+import DSNetReport from '../models/dsnet_report';
+import TimeSeriesResponse from '../models/time_series_response';
+
 export interface NewPeerPayload {
   Owner: string
   Hostname: string
@@ -6,4 +9,13 @@ export interface NewPeerPayload {
 
 export interface AddPeerResponse {
   Conf: string
+}
+
+export interface ReportResponse {
+  Report: DSNetReport,
+  TimeSeries: TimeSeriesResponse,
+}
+
+export interface ErrorResponse {
+  Error: string
 }
